0b1815ed42a2991a1ff993727acc99bb15e036c1
Pranav Tiwari <pranav@Pranavs-MacBook-Pro.local>
2019-01-25 09:16:52 +0530

    added miligram for styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
     <Main />
   </div>
 </template>
